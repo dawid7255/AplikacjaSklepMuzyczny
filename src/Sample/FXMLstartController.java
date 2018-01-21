b2402bf70901b52ccf9433b4657da91dd7fe590e
dawid7255 <33196440+dawid7255@users.noreply.github.com>
2018-01-21 14:10:50 +0100

    ScenaStartowa

---
+++ src/Sample/FXMLstartController.java
@@ -0,0 +1,69 @@
+package Sample;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXMLstartController implements Initializable {
+
+    @FXML
+    private void STARTAction(ActionEvent event) throws IOException {
+        Main.zmianaSceny("start.fxml");
+    }
+
+    @FXML
+    private void otworz_plikAction(ActionEvent event) throws IOException {
+        /*try {
+            Serializacja.WczytajAction();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @FXML
+    private void zapisz_plikAction(ActionEvent event) throws IOException {
+        /*try {
+            Serializacja.ZapiszAction();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    @FXML
+    private void zamknijAction(ActionEvent event) throws IOException {
+        Platform.exit();
+    }
+
+    @FXML
+    private void dodaj_instrumentyAction(ActionEvent event) throws IOException {
+        Main.zmianaSceny("dodaj_instrument.fxml");
+    }
+
+    @FXML
+    private void wyswietl_instrumentyAction(ActionEvent event) throws IOException {
+        Main.zmianaSceny("wyswietl_instrument.fxml");
+    }
+
+    @FXML
+    private void dodaj_uslugiAction(ActionEvent event) throws IOException {
+        Main.zmianaSceny("dodaj_usluge.fxml");
+    }
+
+    @FXML
+    private void wyswietl_uslugiAction(ActionEvent event) throws IOException {
+        Main.zmianaSceny("wyswietl_uslugi.fxml");
+    }
+
+    @FXML
+    private void omnieAction(ActionEvent event) throws IOException {
+        //onas.messagebox();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+}
---
+++ src/Sample/Main.java
@@ -0,0 +1,31 @@
+package Sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        stage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
+        primaryStage.setTitle("Sklep muzyczny");
+        primaryStage.setScene(new Scene(root, 1024, 600));
+        primaryStage.show();
+    }
+
+    public static void zmianaSceny(String sceneName) throws IOException{
+        Parent root = FXMLLoader.load(Main.class.getResource(sceneName));
+        stage.setScene(new Scene(root, 1024,600));
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
